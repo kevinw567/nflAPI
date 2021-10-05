06292527e49c070bf951c8566585df384cd26362
kevinw567 <kevinw567@gmail.com>
2021-10-05 17:24:43 -0400

    Added code to pass search parameters from sidebar component to the results component

--- frontend/src/results.js
+++ frontend/src/results.js
@@ -5,42 +5,80 @@ export class Results extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        loading: true,
-        results: []
+            loading: false,
+            search_criteria: {
+                name: this.props.name,
+                position: this.props.position,
+                team: this.props.team,
+                category: this.props.category
+            },
+            results: [{
+                    name: "Tom Brady",
+                    position: "QB",
+                    team: "TB"
+                },
+                {
+                    name: "Patrick Mahomes",
+                    position: "QB",
+                    team: "KC"
+                },
+                {
+                    name: "Mac Jones",
+                    position: "QB",
+                    team: "NE"
+            }]
         }
     }
-    
 
     async componentDidMount() {
-        const url = "http://127.0.0.1:5000/stats/nfl/rushing?team=NWE&yds=>-1";
-        const response = await fetch(url);
-        const data = await response.json();
-        this.setState({
-            loading: false,
-            results: data
-        });
+        try {
+            let baseurl = "http://127.0.0.1:5000/stats/nfl/"
+            baseurl += this.state.search_criteria["category"] + "?"
+            for (let prop in this.state.search_criteria) {
+                if (prop && prop !== "" && prop !== "category") {
+                    baseurl += prop + "=" + this.state.search_criteria[prop] + "&";
+                }
+            }
+            baseurl.substring(0, baseurl.length - 1);
+            const url = "http://127.0.0.1:5000/stats/nfl/rushing?team=NWE&yds=>-1";
+            console.log(baseurl);
+            const response = await fetch(url);
+            const data = await response.json();
+            this.setState({
+                loading: false,
+                results: data
+            });
+        }
+
+        catch (err) {
+            return
+        }
     }
 
     render() {
         return (
-            this.state.loading ? (
+            this.state.search_criteria.name !== "" ? (
                 <React.Fragment>
                     <table className = "results-table">
                         <thead>
-                            <th className = "cell"> Name </th>
-                            <th className = "cell"> Position </th>
-                            <th className = "cell"> Team </th> 
+                            <tr>
+                                <th className = "cell"> Name </th>
+                                <th className = "cell"> Position </th>
+                                <th className = "cell"> Team </th>
+                            </tr>
                         </thead>
+                        <div className = "no-results"> No results were found </div>
                     </table>
-                    <div className = "no-results"> No results were found </div> 
                 </React.Fragment>
             ) : (
                 <React.Fragment>
                     <table className = "results-table">
-                        <thead>
-                            <th className = "cell"> Name </th>
-                            <th className = "cell"> Position </th>
-                            <th className = "cell"> Team </th> 
+                            <thead>
+                                <tr>
+                                    <th className = "cell"> Name </th>
+                                    <th className = "cell"> Position </th>
+                                    <th className = "cell"> Team </th>
+                                </tr> 
                         </thead>
                         <tbody>
                             { this.state.results.map((player) => (
@@ -57,3 +95,5 @@ export class Results extends React.Component {
         )
     }
 }
+
+export default Results;
--- frontend/src/sidebar.js
+++ frontend/src/sidebar.js
@@ -1,15 +1,47 @@
 import React from "react";
 import "./sidebar.css";
+import { Results } from "./results";
 
 export class Sidebar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isOpen: true,
-            search_criteria: []
+            name: "",
+            exactMatch: false,
+            team: "",
+            position: "",
+            category: ""
         }
 
         this.collapseSidebar = this.collapseSidebar.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.setName = this.setName.bind(this);
+        this.setPosition = this.setPosition.bind(this);
+        this.setTeam = this.setTeam.bind(this);
+        this.setCategory = this.setCategory.bind(this);
+    }
+
+    setName(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    setExactMatch(event) {
+        if (event.target.exactMatch) {
+            this.setState({ exactMatch: true });
+        }
+    }
+
+    setPosition(event) {
+        this.setState({ position: event.target.value });
+    }
+
+    setTeam(event) {
+        this.setState({ team: event.target.value });
+    }
+
+    setCategory(event) {
+        this.setState({ category: event.target.value });
     }
 
     collapseSidebar() {
@@ -18,46 +50,67 @@ export class Sidebar extends React.Component {
         });
     }
 
+    handleSubmit(event) {
+        event.preventDefault()
+    }
+
     render() {
         if (!this.state.isOpen) {
             return (
                 <div className = "closed-sidebar"> 
-                    <button type = "button" className = "closed-collapse-button" onClick = { () => this.collapseSidebar() }> ||| </button>
+                    <button type="button" className="closed-collapse-button" onClick={() => this.collapseSidebar()}>
+                        <i className = "bi bi-arrow-bar-right" width = "24" height = "24"> </i>
+                    </button>
                 </div>
             )
         }
 
         else {
             return (
-                <div className = "open-sidebar">
-                    <button type = "button" className = "collapse-button" onClick = { () => this.collapseSidebar() }> ||| </button>
-                    <form>
-                        <div className = "sidebar-title">
-                            Searchbar
+                <div className="open-sidebar">
+                    <button type="button" className="collapse-button" onClick={() => this.collapseSidebar()}>
+                        <i className = "bi bi-arrow-bar-left" width = "24" height = "24"> </i>
+                    </button>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <div className = "sidebar-title"> Searchbar </div>
                         </div>
+                        <br/>
+                        <br/>
+                        <br/>
                         <div>
                             <div>
-                                <label className = "category-lab" for = "name"> Name </label>
-                                <br/>
-                                <input type = "text" name = "name" style = {{ width: "100%" }}/>
-                                <br/>
-                                Exact Match <input type = "button" className = "name-button"/>
+                                <label className="category-lab" htmlFor="name">
+                                    Name 
+                                    <br/>
+                                    <input type="text" name="name" value={this.state.name} style={{ width: "100%" }} onChange={this.setName}/>
+                                    
+                                    <div className = "category-label"> Exact Match 
+                                        <input type="button" className="name-button" name = "exactMatch" onChange = {this.setExactMatch} />
+                                    </div>
+                                </label>
                             </div>
                             <div>
-                                Position
-                                <br/>
-                                <input type = "text" name = "position" style = {{ width: "100%" }}/>
+                                <label>
+                                    Position
+                                    <br/>
+                                    <input type="text" name="position" style={{ width: "100%" }} onChange={this.setPosition} />
+                                </label>
                             </div>
                             <div>
-                                Team
-                                <br/>
-                                <input type = "text" name = "name" style = {{ width: "100%" }}/>
+                                <label>
+                                    Team
+                                    <br/>
+                                    <input type="text" name="team" style={{ width: "100%" }} onChange={this.setTeam}/>
+                                </label>
                             </div>
-                            <div style = {{ width: "100%" }}>
-                                Category
-                                <br/>
-                                <select name = "category" style = {{ width: "90%" }}>
-                                    <option disabled selected>  </option>
+                            <div>
+                                <label>
+                                    Category
+                                    <br/>
+                                </label>
+                                <select name="category" style={{ width: "90%" }} onChange={this.setCategory}>
+                                    <option disabled defaultValue>  </option>
                                     <option value = "passing"> Passing </option>
                                     <option value = "rushing"> Rushing</option>
                                     <option value = "receiving"> Receiving</option>
@@ -65,13 +118,21 @@ export class Sidebar extends React.Component {
                                     <option value = "returns"> Returns </option>
                                     <option value = "scrimmage"> Scrimmage </option>
                                     <option value = "scoring"> Scoring </option>
-                                </select>    
+                                </select>
+                                
                             </div>
                         </div>
-                        <input type = "submit" className = "submit-button"/>
+                        <input type="submit" className="submit-button" onClick={() => new Results({
+                            name: this.state.name.toString(),
+                            position: this.state.position.toString(),
+                            team: this.state.team.toString(),
+                            category: this.state.category.toString()
+                        }) }/>
                     </form>
                 </div>
             )
         }
     }
 }
+
+export default Sidebar;
